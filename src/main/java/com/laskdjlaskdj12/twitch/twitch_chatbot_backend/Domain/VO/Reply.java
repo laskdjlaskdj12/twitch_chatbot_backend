8c134d0b8c41b083282a8d119b5edd1d4368706b
InHoeKu <laskdjlaskdj12@gmail.com>
2019-03-21 14:56:51 +0900

    응답 인터페이스 추가

---
+++ src/main/java/com/laskdjlaskdj12/twitch/twitch_chatbot_backend/Domain/VO/Reply.java
@@ -0,0 +1,9 @@
+package com.laskdjlaskdj12.twitch.twitch_chatbot_backend.Domain.VO;
+
+public interface Reply {
+	String getMessage();
+	void setMessage(String message);
+
+	String getKey();
+	void setKey(String message);
+}
