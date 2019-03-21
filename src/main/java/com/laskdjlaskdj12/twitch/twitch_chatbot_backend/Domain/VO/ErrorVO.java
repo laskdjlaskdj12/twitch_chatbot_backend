41b3f410c9ae33b061063e8a75fe9e94460f5190
InHoeKu <laskdjlaskdj12@gmail.com>
2019-03-21 14:25:03 +0900

    에러정보 도메인 추가

--- src/main/java/com/laskdjlaskdj12/twitch/twitch_chatbot_backend/Domain/VO/ErrorVO.java
+++ src/main/java/com/laskdjlaskdj12/twitch/twitch_chatbot_backend/Domain/VO/ErrorVO.java
@@ -1,4 +1,9 @@
 package com.laskdjlaskdj12.twitch.twitch_chatbot_backend.Domain.VO;
 
-public class ErrorVO {
+import lombok.Data;
+
+@Data
+public class ErrorVO implements Reply{
+	private String message;
+	private String key;
 }
