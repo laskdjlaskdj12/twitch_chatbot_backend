c81a44a89ec2ed09d1b621e8a791fcc22d9245fc
InHoeKu <laskdjlaskdj12@gmail.com>
2019-03-21 15:14:25 +0900

    사용하지 않는 Controller와 DAO를 제거

--- src/main/java/com/laskdjlaskdj12/twitch/twitch_chatbot_backend/DAO/LotteryApplyHistoryDAO.java
+++
@@ -1,9 +0,0 @@
-package com.laskdjlaskdj12.twitch.twitch_chatbot_backend.DAO;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class LotteryApplyHistoryDAO {
-
-	public void addApplyHistory
-}
