2aa469188cb3784e7b6004fb3061c1cf542764fa
InHoeKu <laskdjlaskdj12@gmail.com>
2019-03-27 14:49:01 +0900

    set deprecated Unused Test Case

--- src/test/java/com/laskdjlaskdj12/twitch/twitch_chatbot_backend/DAO/ApplyHistoryDAOTest.java
+++ src/test/java/com/laskdjlaskdj12/twitch/twitch_chatbot_backend/DAO/ApplyHistoryDAOTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Deprecated
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplyHistoryDAOTest {
--- src/test/java/com/laskdjlaskdj12/twitch/twitch_chatbot_backend/DAO/EmailDAOTest.java
+++ src/test/java/com/laskdjlaskdj12/twitch/twitch_chatbot_backend/DAO/EmailDAOTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
+@Deprecated
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EmailDAOTest {
