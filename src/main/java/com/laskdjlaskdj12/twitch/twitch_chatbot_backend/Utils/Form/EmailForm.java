671164b6b0579566d5f1699dd1eb6df6e2b350da
InHoeKu <laskdjlaskdj12@gmail.com>
2019-04-01 22:52:13 +0900

    Email Form 변경

--- src/main/java/com/laskdjlaskdj12/twitch/twitch_chatbot_backend/Utils/Form/EmailForm.java
+++ src/main/java/com/laskdjlaskdj12/twitch/twitch_chatbot_backend/Utils/Form/EmailForm.java
@@ -19,10 +19,10 @@ public class EmailForm {
 		stringBuilder.append("안녕하세요 시청자님 \n");
 		stringBuilder.append(creator + "의 시청자 참여 컨텐츠에 당첨이 되셨습니다. \n");
 		stringBuilder.append("시청자님께서는 아래 디스코드 채널 에 참가를 해주시면 됩니다. \n");
-		stringBuilder.append("위 초대 만료시각은 5분입니다. 5분이내로 참여를 부탁드립니디\n\n\n\n.");
-		stringBuilder.append("이 시참 시스템은 알파버전이여서 불안정할수있으니 양해 부탁드립니다.\n\n");
-		stringBuilder.append("만약 이상이 발생하셨다면 viewerMatcher@google.com 으로 메일 부탁드립니다.");
 		stringBuilder.append(discordUrl);
+		stringBuilder.append("진행 특성상 빠른 진행을 위해 5분이내로 참여를 하셔야합니다.\n\n");
+		stringBuilder.append("이 시참 시스템은 알파버전이여서 시스템 불안정이 있을수 있으니 많은 양해 부탁드립니다.\n\n");
+		stringBuilder.append("만약 이상이 발생하셨다면 viewerMatcher@google.com 으로 메일 부탁드립니다.");
 		return stringBuilder.toString();
 	}
 }
