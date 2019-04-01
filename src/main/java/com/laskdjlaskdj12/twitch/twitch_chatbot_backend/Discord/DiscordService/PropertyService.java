61995075d5c254a2806b4ce8852e3893bbd597b6
InHoeKu <laskdjlaskdj12@gmail.com>
2019-04-01 21:13:42 +0900

    change as ClassPathResource

--- src/main/java/com/laskdjlaskdj12/twitch/twitch_chatbot_backend/Discord/DiscordService/PropertyService.java
+++ src/main/java/com/laskdjlaskdj12/twitch/twitch_chatbot_backend/Discord/DiscordService/PropertyService.java
@@ -1,5 +1,6 @@
 package com.laskdjlaskdj12.twitch.twitch_chatbot_backend.Discord.DiscordService;
 
+import ch.qos.logback.core.util.FileUtil;
 import com.google.gson.Gson;
 import org.apache.commons.io.FileUtils;
 import org.springframework.core.io.ClassPathResource;
@@ -7,6 +8,7 @@ import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,7 +21,7 @@ public class PropertyService {
 
 		try {
 //			ResourceUtils.getFile("Discord.config");
-			File propertyFile =  ResourceUtils.getFile("Discord.config");
+			File propertyFile = new ClassPathResource("Discord.config").getFile();
 			jsonContent = FileUtils.readFileToString(propertyFile);
 		} catch (IOException e) {
 			e.printStackTrace();
