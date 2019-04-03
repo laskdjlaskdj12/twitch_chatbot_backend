098f4b4607be5ac173bce17b6b2967ad12d254ff
InHoeKu <laskdjlaskdj12@gmail.com>
2019-04-03 21:09:30 +0900

    시간 표시법 버그 수정

--- src/main/java/com/laskdjlaskdj12/twitch/twitch_chatbot_backend/Utils/Utils.java
+++ src/main/java/com/laskdjlaskdj12/twitch/twitch_chatbot_backend/Utils/Utils.java
@@ -6,12 +6,12 @@ import java.time.format.DateTimeFormatter;
 public class Utils {
 
 	public static LocalDateTime stringToLocalDateTime(String timeFormat) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 		return LocalDateTime.parse(timeFormat, formatter);
 	}
 
 	public static String localDateTimeToString(LocalDateTime localDateTime) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 		return formatter.format(localDateTime);
 	}
 }
